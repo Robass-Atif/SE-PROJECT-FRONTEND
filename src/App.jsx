520cc8d672c4f721cf6d9bca74cce4ae477f21c3
Mohammad Abdullah <ma645431@gmail.com>
2024-09-15 15:09:21 +0500

    Configured Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="text-3xl text-blue-500 font-bold underline text-center">Hello world!</h1>
+        
+    </>
+  )
+}
+
+export default App
