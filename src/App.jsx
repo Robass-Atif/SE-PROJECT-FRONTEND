74f436f8de78d42e283ff7c7d2c27a3795545db6
Muhammad Ahmad <muhammadahmadmughal0987@gmail.com>
2024-09-15 20:31:28 +0500

    sign up / sign in /role

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,24 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Profile from './Components/Robass/profile';
-import Rule from './Components/Robass/rule';
-import MessageSection from './Pages/MessageSection';
-import Services from './Pages/Services';
-
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Profile from "./Components/Robass/profile";
+import Rule from "./Components/Robass/rule";
+import MessageSection from "./Pages/MessageSection";
+import Services from "./Pages/Services";
+import Signup from "./Components/Registration/SignUp";
+import RoleSelection from "./Components/Registration/RoleSelection";
+import SignIn from "./Components/Registration/SignIn/SignIn";
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="signup/role-selection" element={<RoleSelection />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="signin" element={<SignIn />} />
         <Route path="/profile" element={<Profile />} />
         <Route path="/rule" element={<Rule />} />
         <Route path="/message" element={<MessageSection />} />
         <Route path="/services" element={<Services />} />
-
-
       </Routes>
     </Router>
   );
