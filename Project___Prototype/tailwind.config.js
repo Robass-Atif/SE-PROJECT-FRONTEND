08bcc0c6bf2bab8e99a7d61c9bf90bb3ce373df2
Robass Atif <mohammadrobass@gmail.com>
2024-09-15 13:36:44 +0500

    add tailwind and folder structure

---
+++ Project___Prototype/tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // Include all files in src folder
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
