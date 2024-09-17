71674dff496eb20ac6e0da0bdd0fb35c3769e3fd
Muhammad Ahmad <muhammadahmadmughal0987@gmail.com>
2024-09-17 21:56:04 +0500

    Account setting layout

--- src/Components/Routes/SettingsRoutes.jsx
+++ src/Components/Routes/SettingsRoutes.jsx
@@ -14,7 +14,7 @@ const SettingsRoutes = () => {
   return (
     <div className="flex">
       <Sidebar />
-      <div className="flex-1 bg-[#F7F9FC] ml-64 p-6 min-h-screen text-[#333] transition-colors duration-300">
+      <div className="flex-1 bg-[#F7F9FC] lg:ml-64 p-6 min-h-screen text-[#333] transition-colors duration-300">
         <div className="border-[#E1E4E8] bg-white shadow-lg mx-auto p-6 border rounded-lg max-w-4xl">
           <Routes>
             <Route path="profile" element={<ProfileSettings />} />
--- src/Components/Setting/AccountSettings.jsx
+++ src/Components/Setting/AccountSettings.jsx
@@ -11,71 +11,69 @@ const AccountSettings = () => {
   };
 
   return (
-    <div className="flex justify-center items-center bg-white/45 px-4 min-h-screen">
-      <div className="space-y-10 bg-white shadow-md p-8 rounded-lg w-full max-w-3xl lg:max-w-4xl">
-        {/* Heading */}
-        <h2 className="font-bold text-3xl text-center text-gray-900 md:text-4xl">
-          Account Settings
-        </h2>
+    <>
+      {/* Heading */}
+      <h2 className="font-bold text-3xl text-center text-gray-900 md:text-4xl">
+        Account Settings
+      </h2>
 
-        <form onSubmit={handleSubmit} className="space-y-6 md:space-y-8 w-full">
-          {/* Current Password */}
-          <div className="flex flex-col">
-            <label className="font-semibold text-base text-gray-600 md:text-lg">
-              Current Password
-            </label>
-            <input
-              type="password"
-              value={currentPassword}
-              onChange={(e) => setCurrentPassword(e.target.value)}
-              className="border-gray-300 mt-2 p-2 md:p-3 border rounded-lg focus:ring focus:ring-blue-200 w-full focus:outline-none"
-              placeholder="Enter your current password"
-              required
-            />
-          </div>
+      <form onSubmit={handleSubmit} className="space-y-6 md:space-y-8 w-full">
+        {/* Current Password */}
+        <div className="flex flex-col">
+          <label className="font-semibold text-base text-gray-600 md:text-lg">
+            Current Password
+          </label>
+          <input
+            type="password"
+            value={currentPassword}
+            onChange={(e) => setCurrentPassword(e.target.value)}
+            className="border-gray-300 mt-2 p-2 md:p-3 border rounded-lg focus:ring focus:ring-blue-200 w-full focus:outline-none"
+            placeholder="Enter your current password"
+            required
+          />
+        </div>
 
-          {/* New Password */}
-          <div className="flex flex-col">
-            <label className="font-semibold text-base text-gray-600 md:text-lg">
-              New Password
-            </label>
-            <input
-              type="password"
-              value={newPassword}
-              onChange={(e) => setNewPassword(e.target.value)}
-              className="border-gray-300 mt-2 p-2 md:p-3 border rounded-lg focus:ring focus:ring-blue-200 w-full focus:outline-none"
-              placeholder="Enter a new password"
-              required
-            />
-          </div>
+        {/* New Password */}
+        <div className="flex flex-col">
+          <label className="font-semibold text-base text-gray-600 md:text-lg">
+            New Password
+          </label>
+          <input
+            type="password"
+            value={newPassword}
+            onChange={(e) => setNewPassword(e.target.value)}
+            className="border-gray-300 mt-2 p-2 md:p-3 border rounded-lg focus:ring focus:ring-blue-200 w-full focus:outline-none"
+            placeholder="Enter a new password"
+            required
+          />
+        </div>
 
-          {/* Confirm New Password */}
-          <div className="flex flex-col">
-            <label className="font-semibold text-base text-gray-600 md:text-lg">
-              Confirm New Password
-            </label>
-            <input
-              type="password"
-              value={confirmPassword}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-              className="border-gray-300 mt-2 p-2 md:p-3 border rounded-lg focus:ring focus:ring-blue-200 w-full focus:outline-none"
-              placeholder="Confirm your new password"
-              required
-            />
-          </div>
+        {/* Confirm New Password */}
+        <div className="flex flex-col">
+          <label className="font-semibold text-base text-gray-600 md:text-lg">
+            Confirm New Password
+          </label>
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            className="border-gray-300 mt-2 p-2 md:p-3 border rounded-lg focus:ring focus:ring-blue-200 w-full focus:outline-none"
+            placeholder="Confirm your new password"
+            required
+          />
+        </div>
 
-          {/* Submit Button */}
-          <div className="flex justify-center">
-            <button
-              type="submit"
-              className="bg-blue-600 hover:bg-blue-500 px-4 md:px-6 py-2 md:py-3 rounded-lg focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 w-full max-w-xs font-semibold text-white focus:outline-none transition duration-300 ease-in-out"
-            >
-              Change Password
-            </button>
-          </div>
-        </form>
-      </div>
-    </div>
+        {/* Submit Button */}
+        <div className="flex justify-center">
+          <button
+            type="submit"
+            className="bg-blue-600 hover:bg-blue-500 px-4 md:px-6 py-2 md:py-3 rounded-lg focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 w-full max-w-xs font-semibold text-white focus:outline-none transition duration-300 ease-in-out"
+          >
+            Change Password
+          </button>
+        </div>
+      </form>
+    </>
   );
 };
 
