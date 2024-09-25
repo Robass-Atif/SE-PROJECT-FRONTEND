f46c9ff2b33c28fdae27695e1d5df0640ac3cc05
Muhammad Saad Akmal <120157311+saadakmal460@users.noreply.github.com>
2024-09-25 22:08:54 +0500

    Completed Edit Profile

--- src/Components/Profile/EditProfile.jsx
+++ src/Components/Profile/EditProfile.jsx
@@ -2,6 +2,23 @@ import React, { useState, useRef, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../../../firebase'
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import { useNavigate} from 'react-router-dom';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+const getUser = async (user_id) => {
+
+  const response = await fetch(`https://backend-qyb4mybn.b4a.run/profile/user/${user_id}`);
+  if (!response.ok) {
+    throw new Error('Network response was not ok');
+  }
+  return response.json();
+
+}
+
 
 
 const EditProfile = () => {
@@ -16,32 +33,100 @@ const EditProfile = () => {
   const [languageProficiency, setLanguageProficiency] = useState({});
   const [fileProgress, setFileProgress] = useState(0);
   const [fileUploadError, setFileUploadError] = useState(null);
-  const [formData , setFormData] = useState(null)
+  const [formData, setFormData] = useState(null)
   const [isUploading, setIsUploading] = useState(false);
-  const languages = ['Urdu' , 'English', 'Spanish', 'French', 'German', 'Hindi'];
+  const languages = ['Urdu', 'English', 'Spanish', 'French', 'German', 'Hindi'];
   const proficiencies = ['Beginner', 'Intermediate', 'Fluent', 'Native'];
   const skills = ['Electrican', 'Plumbing', 'Carpanter', 'Gardner', 'Other'];
 
   const user_id = '66f2c46b560c53a133c31df9'
+  const navigate = useNavigate()
+  
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ['user', user_id],
+    queryFn: () => getUser(user_id),
+  });
+  useEffect(() => {
+    if (data && data.skills) {
+      setSelectedSkills(data.skills); 
+    }
+    if (data && data.language) {
+
+
+      const languages = data.language.map(lang => lang.name);
+      const proficiencyLevels = data.language.reduce((acc, lang) => {
+        acc[lang.name] = lang.level;
+        return acc;
+      }, {});
+
+      setSelectedLanguages(languages);
+      setLanguageProficiency(proficiencyLevels);
+    }
+  }, [data]);
+
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+
+
+
 
   const onSubmit = (data) => {
     const profileData = {
-      ...formData,
+      ...formData, // Assuming formData is defined elsewhere
       ...data,
       skills: selectedSkills,
       languages: selectedLanguages.map(lang => ({
         name: lang,
-        level: languageProficiency[lang] 
-      })), 
+        level: languageProficiency[lang]
+      })),
     };
-  
-    
+
+
+    axios.patch(`https://backend-qyb4mybn.b4a.run/profile/edit-profile/${user_id}`, profileData)
+      .then(response => {
+        toast.success('Profile updated successfully!', {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+        setTimeout(() => {
+          navigate('/profile');
+        }, 1000);
+
+      })
+      .catch(error => {
+        toast.error(error, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
+      });
+
+
+
+
   };
 
 
   const handleFileUpload = (file) => {
-    
-    setIsUploading(true); // Set uploading flag to true
+
+    setIsUploading(true);
     const storage = getStorage(app);
     const fileName = new Date().getTime() + file.name;
     const fileRef = ref(storage, fileName);
@@ -51,24 +136,23 @@ const EditProfile = () => {
     uploadTask.on('state_changed', (snapshot) => {
       const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
       setFileProgress(Math.round(progress));
-      console.log(Math.round(progress))
+      
 
     }, (error) => {
       setFileUploadError(true);
-      setIsUploading(false); // Reset uploading flag on error
-
+      setIsUploading(false);
     }, () => {
       getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
-        console.log(downloadUrl)
+        
         setFormData((prevFormData) => ({ ...prevFormData, profile_image: downloadUrl }));
-        setIsUploading(false); // Reset uploading flag after upload completes
-        setFile(null); // Reset file state to prevent re-triggering
+        setIsUploading(false);
+        setFile(null);
       });
     });
   };
 
   const handleFileChange = (e) => {
-    
+
     const selectedFile = e.target.files[0];
     if (selectedFile) {
       handleFileUpload(selectedFile); // Call the upload function directly
@@ -121,156 +205,181 @@ const EditProfile = () => {
       setSelectedSkills(selectedSkills.filter((skill) => skill !== item));
     }
   };
+  const imageUrl =
+    formData?.profile_image ||
+    data?.profile_image ||
+    'https://via.placeholder.com/150';
 
   return (
-    <div className="flex justify-center items-center min-h-screen">
-      <div className="p-6 w-full max-w-2xl">
-        <h2 className="text-2xl font-bold mb-4 text-center">Profile</h2>
+    <>
+      <ToastContainer />
+      <div className="flex justify-center items-center min-h-screen">
 
-        <div className="flex justify-center mb-4">
-          <div className="relative">
-            <input
-              type="file"
-              ref={fileInputRef}
-              className="absolute w-full h-full opacity-0"
-              accept="image/*"
-              onChange={handleFileChange}
-            />
-            <img
-              src={profileImage || 'https://via.placeholder.com/150'}
-              alt="Profile"
-              className="w-24 h-24 rounded-full object-cover cursor-pointer mb-4"
-              onClick={handleImageClick}
-            />
-          </div>
-        </div>
+        <div className="p-6 w-full max-w-2xl">
+          <h2 className="text-2xl font-bold mb-4 text-center">Profile</h2>
 
-        <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center">
-          {/* First Name */}
-          <div className="mb-4 w-full">
-            <input
-              className="border-gray-300 p-3 border rounded-lg focus:ring-2 focus:ring-custom-violet w-full text-sm sm:text-base focus:outline-none pr-10"
-              id="firstName"
-              placeholder="Full Name"
-              {...register('firstName', { required: 'Name is required' })}
-            />
-            {errors.firstName && <p className="text-red-500 text-xs italic">{errors.firstName.message}</p>}
-          </div>
-
-
-          {/* Address */}
-          <div className="mb-4 w-full">
-            <input
-              className="border-gray-300 p-3 border rounded-lg focus:ring-2 focus:ring-custom-violet w-full text-sm sm:text-base focus:outline-none pr-10"
-              id="address"
-              placeholder="Address"
-              {...register('address', { required: 'Address is required' })}
-            />
-            {errors.address && <p className="text-red-500 text-xs italic">{errors.address.message}</p>}
-          </div>
-
-          {/* Profile Description */}
-          <div className="mb-4 w-full">
-            <textarea
-              className="border-gray-300 p-3 border rounded-lg focus:ring-2 focus:ring-custom-violet w-full text-sm sm:text-base focus:outline-none"
-              id="profileDescription"
-              placeholder="Profile Description"
-              {...register('profileDescription', { required: 'Profile Description is required' })}
-            />
-            {errors.profileDescription && <p className="text-red-500 text-xs italic">{errors.profileDescription.message}</p>}
-          </div>
-
-          {/* Languages Section */}
-          <div className="mb-4 w-full">
-            <label className="block text-gray-700 text-sm font-bold mb-2">Languages</label>
-            <select
-              className="border-gray-300 p-3 border rounded-lg focus:ring-2 focus:ring-custom-violet w-full text-sm sm:text-base focus:outline-none pr-10"
-              onChange={handleLanguageChange}
-              defaultValue=""
-            >
-              <option value="" disabled>Select a language</option>
-              {languages.map((lang) => (
-                <option key={lang} value={lang}>{lang}</option>
-              ))}
-            </select>
-            <div className="flex flex-wrap gap-2 mt-2">
-              {selectedLanguages.map((lang) => (
-                <div key={lang} className="flex items-center bg-blue-100 text-blue-700 px-3 py-1 rounded-full">
-                  {lang}
-                  <select
-                    className="ml-2 border-gray-300 p-1 rounded-lg"
-                    value={languageProficiency[lang] || ""}
-                    onChange={(e) => handleProficiencyChange(lang, e.target.value)}
-                  >
-                    <option value="" disabled>Select proficiency</option>
-                    {proficiencies.map((level) => (
-                      <option key={level} value={level}>{level}</option>
-                    ))}
-                  </select>
-                  <button
-                    type="button"
-                    className="ml-2 text-red-500"
-                    onClick={() => removeItem(lang, 'language')}
-                  >
-                    &times;
-                  </button>
-                </div>
-              ))}
-            </div>
-          </div>
-
-          {/* Skills Section */}
-          <div className="mb-4 w-full">
-            <label className="block text-gray-700 text-sm font-bold mb-2">Skills</label>
-            <select
-              className="border-gray-300 p-3 border rounded-lg focus:ring-2 focus:ring-custom-violet w-full text-sm sm:text-base focus:outline-none pr-10"
-              onChange={handleSkillChange}
-              defaultValue=""
-            >
-              <option value="" disabled>Select a skill</option>
-              {skills.map((skill) => (
-                <option key={skill} value={skill}>{skill}</option>
-              ))}
-            </select>
-
-            {/* Show input for custom skill when "Other" is selected */}
-            {showCustomSkillInput && (
+          <div className="flex flex-col items-center mb-4">
+            <div className="relative">
               <input
-                type="text"
-                className="border-gray-300 p-2 rounded-lg mt-2 w-full"
-                placeholder="Enter custom skill"
-                value={customSkill}
-                onChange={(e) => setCustomSkill(e.target.value)}
-                onKeyDown={handleCustomSkillInput}
+                type="file"
+                ref={fileInputRef}
+                className="absolute w-full h-full opacity-0"
+                accept="image/*"
+                onChange={handleFileChange}
               />
-            )}
-
-            <div className="flex flex-wrap gap-2 mt-2">
-              {selectedSkills.map((skill) => (
-                <div key={skill} className="flex items-center bg-blue-100 text-blue-700 px-3 py-1 rounded-full">
-                  {skill}
-                  <button
-                    type="button"
-                    className="ml-2 text-red-500"
-                    onClick={() => removeItem(skill, 'skill')}
-                  >
-                    &times;
-                  </button>
-                </div>
-              ))}
+              <img
+                src={imageUrl}
+                alt="Profile"
+                className="w-24 h-24 rounded-full object-cover cursor-pointer mb-2"
+                onClick={handleImageClick}
+              />
+              <span>
+                {fileUploadError ? (
+                  <span className="text-red-500">Error Uploading File</span>
+                ) : fileProgress > 0 && fileProgress < 100 ? (
+                  <span className="text-slate-500">{`Uploading ${fileProgress}%`}</span>
+                ) : fileProgress === 100 ? (
+                  <span className="text-green-600">Image Updated</span>
+                ) : null}
+              </span>
             </div>
           </div>
 
-          {/* Submit Button */}
-          <button
-            type="submit"
-            className="bg-custom-violet text-white px-4 py-2 rounded-lg hover:bg-violet-700 transition duration-300"
-          >
-            Save Changes
-          </button>
-        </form>
+
+
+
+          <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col items-center">
+            {/* First Name */}
+            <div className="mb-4 w-full">
+              <input
+                className="border-gray-300 p-3 border rounded-lg focus:ring-2 focus:ring-custom-violet w-full text-sm sm:text-base focus:outline-none pr-10"
+                id="name"
+                placeholder="Full Name"
+                defaultValue={data.name ? data.name : ''}
+                {...register('name', { required: 'Name is required' })}
+              />
+              {errors.name && <p className="text-red-500 text-xs italic">{errors.name.message}</p>}
+            </div>
+
+
+            {/* Address */}
+            <div className="mb-4 w-full">
+              <input
+                className="border-gray-300 p-3 border rounded-lg focus:ring-2 focus:ring-custom-violet w-full text-sm sm:text-base focus:outline-none pr-10"
+                id="address"
+                placeholder="Address"
+                defaultValue={data.location ? data.location : ''}
+
+                {...register('address', { required: 'Address is required' })}
+              />
+              {errors.address && <p className="text-red-500 text-xs italic">{errors.address.message}</p>}
+            </div>
+
+            {/* Profile Description */}
+            <div className="mb-4 w-full">
+              <textarea
+                className="border-gray-300 p-3 border rounded-lg focus:ring-2 focus:ring-custom-violet w-full text-sm sm:text-base focus:outline-none"
+                id="profileDescription"
+                placeholder="Profile Description"
+                defaultValue={data.profile_description ? data.profile_description : ''}
+
+                {...register('profileDescription', { required: 'Profile Description is required' })}
+              />
+              {errors.profileDescription && <p className="text-red-500 text-xs italic">{errors.profileDescription.message}</p>}
+            </div>
+
+            {/* Languages Section */}
+            <div className="mb-4 w-full">
+              <label className="block text-gray-700 text-sm font-bold mb-2">Languages</label>
+              <select
+                className="border-gray-300 p-3 border rounded-lg focus:ring-2 focus:ring-custom-violet w-full text-sm sm:text-base focus:outline-none pr-10"
+                onChange={handleLanguageChange}
+                defaultValue=""
+              >
+                <option value="" disabled>Select a language</option>
+                {languages.map((lang) => (
+                  <option key={lang} value={lang}>{lang}</option>
+                ))}
+              </select>
+              <div className="flex flex-wrap gap-2 mt-2">
+                {selectedLanguages.map((lang) => (
+                  <div key={lang} className="flex items-center bg-blue-100 text-blue-700 px-3 py-1 rounded-full">
+                    {lang}
+                    <select
+                      className="ml-2 border-gray-300 p-1 rounded-lg"
+                      value={languageProficiency[lang] || ""}
+                      onChange={(e) => handleProficiencyChange(lang, e.target.value)}
+                    >
+                      <option value="" disabled>Select proficiency</option>
+                      {proficiencies.map((level) => (
+                        <option key={level} value={level}>{level}</option>
+                      ))}
+                    </select>
+                    <button
+                      type="button"
+                      className="ml-2 text-red-500"
+                      onClick={() => removeItem(lang, 'language')}
+                    >
+                      &times;
+                    </button>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            {/* Skills Section */}
+            <div className="mb-4 w-full">
+              <label className="block text-gray-700 text-sm font-bold mb-2">Skills</label>
+              <select
+                className="border-gray-300 p-3 border rounded-lg focus:ring-2 focus:ring-custom-violet w-full text-sm sm:text-base focus:outline-none pr-10"
+                onChange={handleSkillChange}
+                defaultValue=""
+              >
+                <option value="" disabled>Select a skill</option>
+                {skills.map((skill) => (
+                  <option key={skill} value={skill}>{skill}</option>
+                ))}
+              </select>
+
+              {/* Show input for custom skill when "Other" is selected */}
+              {showCustomSkillInput && (
+                <input
+                  type="text"
+                  className="border-gray-300 p-2 rounded-lg mt-2 w-full"
+                  placeholder="Enter custom skill"
+                  value={customSkill}
+                  onChange={(e) => setCustomSkill(e.target.value)}
+                  onKeyDown={handleCustomSkillInput}
+                />
+              )}
+
+              <div className="flex flex-wrap gap-2 mt-2">
+                {selectedSkills.map((skill) => (
+                  <div key={skill} className="flex items-center bg-blue-100 text-blue-700 px-3 py-1 rounded-full">
+                    {skill}
+                    <button
+                      type="button"
+                      className="ml-2 text-red-500"
+                      onClick={() => removeItem(skill, 'skill')}
+                    >
+                      &times;
+                    </button>
+                  </div>
+                ))}
+              </div>
+            </div>
+
+            {/* Submit Button */}
+            <button
+              type="submit"
+              className="bg-custom-violet text-white px-4 py-2 rounded-lg hover:bg-violet-700 transition duration-300"
+            >
+              Save Changes
+            </button>
+          </form>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
